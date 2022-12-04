b448e5c1b8a73c1adb11d3971cc4732c76a8b2ea
Paul Horn <dev@knutwalker.engineer>
2022-12-04 16:53:53 +0100

    Solve day 4

---
+++ src/bin/aoc2022/day4.rs
@@ -0,0 +1,92 @@
+use std::ops::{RangeBounds, RangeInclusive};
+
+type Output = usize;
+
+register!(
+    "input/day4.txt";
+    (input: input!(Input)) -> Output {
+        part1(&input);
+        part2(&input);
+    }
+);
+
+fn part1(items: &[Input]) -> Output {
+    items
+        .iter()
+        .filter(|Input([r1, r2])| {
+            r1.contains(r2.start()) && r1.contains(r2.end())
+                || r2.contains(r1.start()) && r2.contains(r1.end())
+        })
+        .count()
+}
+
+fn part2(items: &[Input]) -> Output {
+    items
+        .iter()
+        .filter(|Input([r1, r2])| r1.contains(r2.end()) || r2.contains(r1.end()))
+        .count()
+}
+
+pub struct Input([RangeInclusive<u32>; 2]);
+
+impl From<&str> for Input {
+    fn from(input: &str) -> Self {
+        (|| {
+            let (start, input) = input.split_once('-')?;
+            let (end, input) = input.split_once(',')?;
+            let range1 = start.parse().ok()?..=end.parse().ok()?;
+            let (start, end) = input.split_once('-')?;
+            let range2 = start.parse().ok()?..=end.parse().ok()?;
+            Some(Self([range1, range2]))
+        })()
+        .unwrap()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use aoc::{Solution, SolutionExt};
+    use test::Bencher;
+
+    #[test]
+    fn test_ex() {
+        let input = r#"
+        2-4,6-8
+        2-3,4-5
+        5-7,7-9
+        2-8,3-7
+        6-6,4-6
+        2-6,4-8
+        "#;
+        let (res1, res2) = Solver::run_on(input);
+        assert_eq!(res1, 2);
+        assert_eq!(res2, 4);
+    }
+
+    #[test]
+    fn test() {
+        let (res1, res2) = Solver::run_on_input();
+        assert_eq!(res1, 444);
+        assert_eq!(res2, 801);
+    }
+
+    #[bench]
+    fn bench_parsing(b: &mut Bencher) {
+        let input = Solver::puzzle_input();
+        b.bytes = input.len() as u64;
+        b.iter(|| Solver::parse_input(input));
+    }
+
+    #[bench]
+    fn bench_pt1(b: &mut Bencher) {
+        let input = Solver::parse_input(Solver::puzzle_input());
+        b.iter(|| part1(&input));
+    }
+
+    #[bench]
+    fn bench_pt2(b: &mut Bencher) {
+        let input = Solver::parse_input(Solver::puzzle_input());
+        b.iter(|| part2(&input));
+    }
+}
--- src/bin/aoc2022/main.rs
+++ src/bin/aoc2022/main.rs
@@ -35,7 +35,7 @@ aoc_main!(
     1 => day1,
     2 => day2,
     3 => day3,
-    // 4 => day4,
+    4 => day4,
     // 5 => day5,
     // 6 => day6,
     // 7 => day7,
