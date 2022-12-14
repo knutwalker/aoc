e56b2328652986a10fe36ba6f606ef42a7890c9f
Paul Horn <dev@knutwalker.engineer>
2022-12-14 19:36:11 +0100

    Improve day 14

--- src/bin/aoc2022/day14.rs
+++ src/bin/aoc2022/day14.rs
@@ -1,238 +1,135 @@
-use aoc::{MinMax, Parse, ProcessInput};
-use fxhash::FxHashSet;
+use aoc::{MinMax, Parse, PuzzleInput};
+use bitvec::prelude::{bitarr, BitArray, Lsb0};
 
 type Output = usize;
 
 register!(
     "input/day14.txt";
-    (input: input!(process Map)) -> Output {
+    (input: input!(verbatim Map)) -> Output {
         part1(&mut input);
         part2(&mut input);
     }
 );
 
+const SPAWN: Coord = Coord { x: 500, y: 0 };
+
+macro_rules! try_move {
+    ($blocked:ident, $sand:ident) => {
+        // try down
+        let down = $sand.move_y(1);
+        if $blocked.is_free(down) {
+            $sand = down;
+            continue;
+        }
+
+        // try down-left
+        let down_left = down.move_x(-1);
+        if $blocked.is_free(down_left) {
+            $sand = down_left;
+            continue;
+        }
+
+        // try down-right
+        let down_right = down.move_x(1);
+        if $blocked.is_free(down_right) {
+            $sand = down_right;
+            continue;
+        }
+
+        break $sand;
+    };
+}
+
 fn part1(map: &mut Map) -> Output {
-    let min_max_x = map.x_bounds;
-    let min_max_y = map.y_bounds;
+    let left = map.x_bounds.min;
+    let right = map.x_bounds.max;
+    let bottom = map.y_bounds.max;
 
     let blocked = &mut map.blocked;
-
-    let mut drops = 0;
+    let mut drops = map.drops;
 
     loop {
-        let mut sand = Coord { x: 500, y: 0 };
+        let mut sand = SPAWN;
 
         let resting = loop {
-            if sand.x < min_max_x.min || sand.x > min_max_x.max || sand.y > min_max_y.max {
+            if sand.x < left || sand.x > right || sand.y > bottom {
                 map.drops = drops;
                 return drops;
             }
 
-            // try down
-            let down = sand.move_y(1);
-            if !blocked.contains(&down) {
-                sand = down;
-                continue;
-            }
-
-            // try down-left
-            let down_left = down.move_x(-1);
-            if !blocked.contains(&down_left) {
-                sand = down_left;
-                continue;
-            }
-
-            // try down-right
-            let down_right = down.move_x(1);
-            if !blocked.contains(&down_right) {
-                sand = down_right;
-                continue;
-            }
-
-            break sand;
+            try_move!(blocked, sand);
         };
 
         drops += 1;
-        blocked.insert(resting);
+        blocked.block(resting);
     }
 }
 
 fn part2(map: &mut Map) -> Output {
     let bottom = map.y_bounds.max + 1;
-    let blocked = &mut map.blocked;
 
+    let blocked = &mut map.blocked;
     let mut drops = map.drops;
-    let spawn = Coord { x: 500, y: 0 };
 
     loop {
-        let mut sand = spawn;
+        let mut sand = SPAWN;
 
         let resting = loop {
             if sand.y == bottom {
                 break sand;
             }
 
-            // try down
-            let down = sand.move_y(1);
-            if !blocked.contains(&down) {
-                sand = down;
-                continue;
-            }
-
-            // try down-left
-            let down_left = down.move_x(-1);
-            if !blocked.contains(&down_left) {
-                sand = down_left;
-                continue;
-            }
-
-            // try down-right
-            let down_right = down.move_x(1);
-            if !blocked.contains(&down_right) {
-                sand = down_right;
-                continue;
-            }
-
-            break sand;
+            try_move!(blocked, sand);
         };
 
         drops += 1;
 
-        if resting == spawn {
+        if resting == SPAWN {
             return drops;
         }
 
-        blocked.insert(resting);
+        blocked.block(resting);
     }
 }
 
 #[derive(Clone)]
 pub struct Map {
-    blocked: FxHashSet<Coord>,
+    blocked: Cave,
     x_bounds: MinMax<u32>,
     y_bounds: MinMax<u32>,
     drops: usize,
 }
 
-impl ProcessInput for Map {
-    type In = input!(Input);
+#[derive(Clone)]
+struct Cave(BitArray<[u64; 1250]>);
 
-    type Out<'a> = Self;
+impl Cave {
+    // actual boundaries are about [300,700] for x and [0, 200] for y
+    const MIN_X: u32 = 300;
+    const WIDTH: u32 = 400;
+    const HEIGHT: u32 = 200;
 
-    fn process(input: <Self::In as aoc::PuzzleInput>::Out<'_>) -> Self::Out<'_> {
-        let blocked = input
-            .iter()
-            .flat_map(|i| {
-                i.path
-                    .iter()
-                    .scan((0, 0), |prev, end| {
-                        Some({
-                            if matches!(*prev, (0, 0)) {
-                                *prev = (end.x, end.y);
-                            }
+    fn new() -> Self {
+        let size = Self::WIDTH * Self::HEIGHT;
+        assert_eq!(size, 80_000);
 
-                            let (start_x, start_y) = *prev;
-                            *prev = (end.x, end.y);
-
-                            if start_x == end.x {
-                                let dy = (end.y as i32).saturating_sub(start_y as i32).signum();
-                                Line::x_line(
-                                    Coord {
-                                        x: start_x,
-                                        y: start_y,
-                                    },
-                                    end.y,
-                                    dy,
-                                )
-                            } else {
-                                let dx = (end.x as i32).saturating_sub(start_x as i32).signum();
-                                Line::y_line(
-                                    Coord {
-                                        x: start_x,
-                                        y: start_y,
-                                    },
-                                    end.x,
-                                    dx,
-                                )
-                            }
-                        })
-                    })
-                    .flatten()
-            })
-            .collect();
-
-        let x_bounds = input
-            .iter()
-            .flat_map(|i| i.path.iter().map(|p| p.x))
-            .collect();
-        let y_bounds = input
-            .iter()
-            .flat_map(|i| i.path.iter().map(|p| p.y))
-            .collect();
-
-        Self {
-            blocked,
-            x_bounds,
-            y_bounds,
-            drops: 0,
-        }
-    }
-}
-
-#[derive(Debug)]
-struct Line {
-    start: Option<Coord>,
-    end: Coord,
-    dx: i32,
-    dy: i32,
-}
-
-impl Line {
-    fn x_line(start: Coord, end_y: u32, dy: i32) -> Self {
-        Self {
-            start: Some(start),
-            end: Coord {
-                x: start.x,
-                y: end_y,
-            },
-            dx: 0,
-            dy,
-        }
+        Self(bitarr![u64, Lsb0; 0; 80_000])
     }
 
-    fn y_line(start: Coord, end_x: u32, dx: i32) -> Self {
-        Self {
-            start: Some(start),
-            end: Coord {
-                x: end_x,
-                y: start.y,
-            },
-            dx,
-            dy: 0,
-        }
+    fn to_index(coord: Coord) -> u32 {
+        let x = coord.x - Self::MIN_X;
+        coord.y * Self::WIDTH + x
     }
-}
 
-impl Iterator for Line {
-    type Item = Coord;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let mut start = self.start.as_mut()?;
-        if *start == self.end {
-            self.start = None;
-            return Some(self.end);
-        }
-
-        start.x = start.x.saturating_add_signed(self.dx);
-        start.y = start.y.saturating_add_signed(self.dy);
-
-        self.start
+    fn is_free(&self, coord: Coord) -> bool {
+        let index = Self::to_index(coord);
+        !self.0[index as usize]
     }
-}
 
-#[derive(Clone, Debug)]
-pub struct Input {
-    path: Vec<Coord>,
+    fn block(&mut self, coord: Coord) {
+        let index = Self::to_index(coord);
+        self.0.set(index as usize, true);
+    }
 }
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
@@ -257,16 +154,6 @@ impl Coord {
     }
 }
 
-impl Parse for Input {
-    type Out<'a> = Self;
-
-    fn parse_from(input: &str) -> Self::Out<'_> {
-        Self {
-            path: input.split(" -> ").map(Coord::parse_from).collect(),
-        }
-    }
-}
-
 impl Parse for Coord {
     type Out<'a> = Self;
 
@@ -275,6 +162,78 @@ impl Parse for Coord {
         Self {
             x: x.parse().unwrap(),
             y: y.parse().unwrap(),
+        }
+    }
+}
+
+impl PuzzleInput for Map {
+    type Out<'a> = Self;
+
+    fn from_input(input: &str) -> Self::Out<'_> {
+        const SENTINEL: Coord = Coord { x: 0, y: 0 };
+
+        let mut min_x = u32::MAX;
+        let mut max_x = 0;
+        let mut max_y = 0;
+
+        let mut blocked = Cave::new();
+
+        for line in aoc::lines(input) {
+            let mut prev = SENTINEL;
+            for coord in line.split(" -> ") {
+                let coord = Coord::parse_from(coord);
+
+                if coord.x < min_x {
+                    min_x = coord.x;
+                }
+                if coord.x > max_x {
+                    max_x = coord.x;
+                }
+                if coord.y > max_y {
+                    max_y = coord.y;
+                }
+
+                if prev == SENTINEL {
+                    prev = coord;
+                    continue;
+                }
+
+                #[allow(clippy::comparison_chain)]
+                if prev.x == coord.x {
+                    if prev.y <= coord.y {
+                        for y in prev.y..=coord.y {
+                            blocked.block(Coord { x: coord.x, y });
+                        }
+                    } else {
+                        for y in coord.y..=prev.y {
+                            blocked.block(Coord { x: coord.x, y });
+                        }
+                    };
+                } else if prev.x < coord.x {
+                    for x in prev.x..=coord.x {
+                        blocked.block(Coord { x, y: coord.y });
+                    }
+                } else {
+                    for x in coord.x..=prev.x {
+                        blocked.block(Coord { x, y: coord.y });
+                    }
+                }
+
+                prev = coord;
+            }
+        }
+
+        let x_bounds = MinMax {
+            min: min_x,
+            max: max_x,
+        };
+        let y_bounds = MinMax { min: 0, max: max_y };
+
+        Self {
+            blocked,
+            x_bounds,
+            y_bounds,
+            drops: 0,
         }
     }
 }
@@ -318,7 +277,8 @@ mod tests {
 
     #[bench]
     fn bench_pt2(b: &mut Bencher) {
-        let input = Solver::parse_input(Solver::puzzle_input());
+        let mut input = Solver::parse_input(Solver::puzzle_input());
+        part1(&mut input);
         b.iter(|| part2(&mut input.clone()));
     }
 }
